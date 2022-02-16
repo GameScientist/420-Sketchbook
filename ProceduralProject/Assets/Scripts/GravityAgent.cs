4594e2778914f34bd048e20f61e2d82acaf73fc1
Tyler Kortge <tylerkortge@charter.net>
2022-02-16 12:43:09 -0500

    Modified scene to have a lot more planets.

--- ProceduralProject/Assets/Scripts/GravityAgent.cs
+++ ProceduralProject/Assets/Scripts/GravityAgent.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class GravityAgent : MonoBehaviour
 {
     static float G = 1;
-    static float MAX_FORCE = 1;
+    static float MAX_FORCE = 100;
     static List<GravityAgent> agents = new List<GravityAgent>();
+
+    public bool sun;
     static void FindGravityForce(GravityAgent a, GravityAgent b)
     {
-        if (a == b) return;
-        if (a.isDone) return;
-        if (b.isDone) return;
+        if (a == b || a.isDone || b.isDone) return;
 
         Vector3 vectorToB = b.position - a.position;
-        float gravity = G * (a.mass * b.mass) / vectorToB.sqrMagnitude;
+        float gravity
+            = G
+            * (a.mass * b.mass)
+            / vectorToB.sqrMagnitude;
 
-        if(gravity > MAX_FORCE) gravity = MAX_FORCE;
+        if (gravity > MAX_FORCE) gravity = MAX_FORCE;
 
         vectorToB.Normalize();
 
@@ -34,34 +37,44 @@ public class GravityAgent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (sun)
+        {
+            SetParameters(Vector3.zero, 1000, Color.yellow);
+            for (int i = 0; i < 30; i++) Instantiate(gameObject).GetComponent<GravityAgent>().sun = false;
+        }
+        else SetParameters(new Vector3(
+            Random.Range(-54, 54),
+            Random.Range(-54, 54),
+            Random.Range(-54, 54)),
+            Random.Range(10f, 100f),
+            new Color(
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f)));
+    }
+    private void SetParameters(Vector3 startingPosition, float startingMass, Color color)
+    {
+        transform.position = startingPosition;
         position = transform.position;
-        mass = Random.Range(10, 100);
+        mass = startingMass;
+        transform.localScale = Vector3.one * Mathf.Sqrt(mass);
+        GetComponent<Renderer>().material.color = color;
         agents.Add(this);
     }
-    private void OnDestroy()
-    {
-        agents.Remove(this);
-    }
+    private void OnDestroy() => agents.Remove(this);
 
-    public void AddForce(Vector3 f)
-    {
-        force += f;
-    }
+    public void AddForce(Vector3 f) => force += f;
 
     // Update is called once per frame
     void Update()
     {
         // calc gravity to every other agent:
         foreach (GravityAgent a in agents)
-        {
             FindGravityForce(this, a);
-        }
         isDone = true;
 
         // euler integration:
-        Vector3 acceleration = force / mass;
-
-        velocity += acceleration * Time.deltaTime;
+        velocity += force / mass * Time.deltaTime;
         position += velocity * Time.deltaTime;
 
         transform.position = position;
