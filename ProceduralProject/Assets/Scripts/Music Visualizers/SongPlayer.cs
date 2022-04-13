c5b030fb7b4a220b70b1000e7b161d3ee6496219
Tyler Kortge <tylerkortge@charter.net>
2022-04-13 11:08:37 -0400

    Week 13 Music Lab

---
+++ ProceduralProject/Assets/Scripts/Music Visualizers/SongPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(AudioSource))]
+public class SongPlayer : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    private AudioSource player;
+    private int currentTrack = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<AudioSource>();
+        PlayTrack(0);
+    }
+
+    public void PlayTrack(int n)
+    {
+        if (n < 0 || n >= playlist.Length) return;
+        player.PlayOneShot(playlist[n]);
+        currentTrack = n;
+    }
+
+    public void PlayTrackRandom()
+    {
+        PlayTrack(Random.Range(0, playlist.Length));
+    }
+
+    public void PlayTrackNext()
+    {
+        int track = currentTrack + 1;
+        if (track >= playlist.Length) track = 0;
+
+        PlayTrack(track);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player.isPlaying)
+        {
+            PlayTrackNext();
+        }
+    }
+}
+
+//[CustomEditor(typeof(SongPlayer))]
+//public class Mus
